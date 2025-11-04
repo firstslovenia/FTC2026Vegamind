449c8d0431b781199ec2be36f64da989b475e11d
Jakob Tadej Vrtacnik <jakobtadejv@gmail.com>
2025-11-04 11:32:54 +0100

    [UPD] Removed redundant magazine gate function from shooter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shooter/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shooter/Shooter.java
@@ -1,15 +1,11 @@
 package org.firstinspires.ftc.teamcode.shooter;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.Servo;
 
 public class Shooter {
     private DcMotor motor;
-    private Servo magazine;
-    public Shooter(DcMotor motor, Servo magazine) {
+    public Shooter(DcMotor motor) {
        this.motor = motor;
-       this.magazine = magazine;
 
        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
     }
@@ -24,16 +20,6 @@ public class Shooter {
 
     public void winddown() {
         motor.setPower(0.0f);
-    }
-
-    public void shoot(Gamepad gamepad) {
-        motor.setPower(gamepad.left_trigger - gamepad.right_trigger);
-
-        if(gamepad.dpad_left) {
-            magazine.setPosition(-1.0f);
-        } else if (gamepad.dpad_right) {
-            magazine.setPosition(1.0f);
-        }
     }
 }
 
