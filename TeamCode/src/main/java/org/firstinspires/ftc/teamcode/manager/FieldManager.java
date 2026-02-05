13fb36d240bf449765f8200f8c957c7c8586bf05
kweezo <jakobtadejv@gmail.com>
2026-02-05 20:52:05 +0100

    [ADD][WIP] Methods to calculate ball positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/manager/FieldManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/manager/FieldManager.java
@@ -1,8 +1,8 @@
 package org.firstinspires.ftc.teamcode.manager;
 
+import android.util.Pair;
+
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.teamcode.color.BallColor;
-import org.firstinspires.ftc.teamcode.vision.BallContour;
 import org.firstinspires.ftc.teamcode.vision.BallPipeline;
 
 import java.util.ArrayList;
@@ -13,47 +13,63 @@ public class FieldManager {
 
     final int FIELD_SIZE = 366; //cm
 
-    int viewWidth, viewHeight, viewOffsetX, viewOffsetY;
+    List<Pair<Double, Double>> ballFieldPos;
+
+    double camOffsetX, camOffsetY, horFov, verFov;
+    double streamWidth, streamHeight;
 
     BallPipeline pipeline;
 
-    public FieldManager(WebcamName webcamName, int viewWidth, int viewHeight, int viewOffsetX, int viewOffsetY, int streamWidth, int streamHeight) {
-        this.viewWidth = viewWidth;
-        this.viewHeight = viewHeight;
-        this.viewOffsetX = viewOffsetX;
-        this.viewOffsetY = viewOffsetY;
+    public FieldManager(WebcamName webcamName, double streamWidth, double streamHeight, double camOffsetX, double camOffsetY, double fov) {
+        this.camOffsetX = camOffsetX;
+        this.camOffsetY = camOffsetY;
+        this.streamWidth = streamWidth;
+        this.streamHeight = streamHeight;
+        this.horFov = fov;
+        this.verFov = (streamHeight / streamWidth) * fov;
 
         //pipeline = new BallPipeline(webcamName, streamWidth, streamHeight);
     }
 
-    void update(int robotX, int robotY) {
-       List<BallContour> ballContours = pipeline.getBallContours();
-        BallColor currColor = pipeline.getLastUpdateColor();
+    double computeBallPosX(FieldBall ball, double pitch) {
+        double x1 = camOffsetX; // left point of triangle
 
-       if(ballContours == null || ballContours.isEmpty()) return;
+        double length = Math.tan(pitch + horFov) * camOffsetY; // length of the bottom side of the view
 
-       int viewCenterX = robotX + viewOffsetX; // relative to the field
-       int viewCenterY = robotY + viewOffsetY;
+        double x2 = x1 + length; // right point of triangle
 
-        for(int i = 0; i < balls.size(); i++) { // remove all the balls we thought were in that area, as we dont know if they are anymore, we'll just replace them with what
-            // we can see, and yes im just iterating through since the n is small
-            FieldBall ball = balls.get(i);
+        //also x and y are flipped because the camera stream is flipped
+        double relativeBallPosX = Math.cos(ball.y / streamHeight/*normalize*/ * 3.14159 / 2 /*so we get normalized output*/);//normalized ball position relative to triangle, this calc is to account for distortion near the dges
+        //also it is of note cos is used here because the distortion isn't linear
 
-            if(ball.x < viewCenterX - viewWidth / 2.0 || ball.x > viewCenterX + viewWidth / 2.0 || // should we account for radius, also what about balls only partially in view?
-               ball.y < viewCenterY - viewHeight / 2.0 || ball.y > viewCenterY + viewHeight / 2.0) {
-                continue; //ball outside view range, we wont be updating this part so we leave as is
-            }
+        double scaledBallPosX = relativeBallPosX * length + x1;
 
-            balls.remove(i);
-            i--;
-        }
-
-        for(BallContour ballContour : ballContours) {
-            balls.add(
-                    new FieldBall(ballContour.x * viewWidth + viewCenterX,
-                                  ballContour.y * viewHeight + viewCenterY,
-                                     currColor)
-            );
-        }
+        return scaledBallPosX;
     }
+
+    double computeBallPosY(FieldBall ball, double pitch) {
+        double trapezoidOffsetX = Math.sin(pitch) * camOffsetY;
+
+        double bottomLength = camOffsetY * 2 * Math.tan(horFov / 2); // get the bottom line length of the trapezoid
+        //first we halve the horFov to get the right triangle then we double it again to get the entire length
+
+        double sideLength = camOffsetY * Math.tan(verFov);// the hypotenuse of the triangle with points
+        //A: the far point of the bottom line
+        //B: far point of the bottom line + camOffsetY
+        //C: far point on the top line
+
+        double topLength = bottomLength + 2 * sideLength * Math.sin(3.14159/2 - horFov / 2);
+        double height = sideLength * Math.cos(90 - horFov / 2); // using the side length we can make 2 right triangles at the
+        //corners of the trapezoid, from that figure out their length on the part where they cover the top length of the trapezoid.
+        //so that * 2 + bottomLineLength = topLineLength
+        //we will use the height for interpolation
+
+        //note for now ballPosY does not take the pitch into account as of now!!
+
+        double ballPosY = trapezoidOffsetX + (ball.x / streamWidth) * topLength;
+
+        return ballPosY;
+    }
+
+
 }
